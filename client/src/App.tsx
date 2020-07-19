2e582998086e08c6b1e5fbcde6ff8a529fd65a81
Graham Miller <grahamiller96@gmail.com>
2020-07-19 00:02:16 -0400

    feat: adding moment package, making countdown

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,73 @@
 import React, { useState } from "react";
+import moment, { Moment } from "moment";
 import "./App.css";
 import { useInterval } from "./useInterval";
 
-function getTimeTill2021(currentDate: Date, countDownDate: Date) {
-  // Get today's date and time
-  const monthsLeft = 12 - currentDate.getMonth();
+interface TimeLeft {
+  months: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
+function getTimeTill2021(currentDate: Moment, countDownDate: Moment): TimeLeft {
+  // Get currentDate/countDownDate diff
+  const months = countDownDate.diff(currentDate, "months");
+  currentDate.month(11);
+  const distance = countDownDate.diff(currentDate);
   // Time calculations for days, hours, minutes and seconds
-  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  return {
+    months: months,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+}
+
+function pluralize(stringToPluralize: string, pluralizedFor: number): string {
+  if (pluralizedFor !== 1) {
+    return stringToPluralize + "s";
+  }
+  return stringToPluralize;
 }
 
 function App() {
-  let [count, setCount] = useState(0);
-  const countDownDate = new Date("Jan 1, 2021 00:00:00");
-  const currentDate = new Date();
+  const countDownDate = moment("2020-12-31T23:59:59");
+
+  let [timeLeft, setTimeLeft] = useState<TimeLeft>(
+    getTimeTill2021(moment(), countDownDate)
+  );
 
   useInterval(() => {
-    setCount(count + 1);
+    setTimeLeft(getTimeTill2021(moment(), countDownDate));
   });
 
   return (
     <div className="App">
-      <header className="App-header">Hello {count} </header>
+      <p className="timerRow">
+        {timeLeft.months} {pluralize("month", timeLeft.months)}
+      </p>
+      <p className="timerRow">
+        {timeLeft.days} {pluralize("day", timeLeft.days)}
+      </p>
+      <p className="timerRow">
+        {timeLeft.hours} {pluralize("hour", timeLeft.hours)}
+      </p>
+      <p className="timerRow">
+        {timeLeft.minutes} {pluralize("minute", timeLeft.minutes)}
+      </p>
+      <p className="timerRow">
+        {timeLeft.seconds} {pluralize("second", timeLeft.seconds)}
+      </p>
     </div>
   );
 }
